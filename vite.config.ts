9dd81e6de4557d440ecfcbeef8cd5a76a796886c
CC11001100 <CC11001100@qq.com>
2025-07-13 19:36:06 +0000

    Deploying to gh from @ CC11001100/wx-bug-editor@0f3e26ac305d4bc4239b49552c3507233e3f15f3 🚀

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/wx-bug-editor/', // GitHub Pages 仓库名称
-})
